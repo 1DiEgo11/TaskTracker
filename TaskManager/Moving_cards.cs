96f1b64b968f2ae4204301d38dc6fef5aa935442
1DiEgo11 <sona.egor23@gmail.com>
2023-05-09 13:26:26 +1000

    Тест перенос карточки

---
+++ TaskManager/Moving_cards.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TaskManager
+{
+    internal class Moving_cards
+    {
+        public static void Move(StackPanel content)
+        {
+            Border s = (Border)content.Children[0];
+            StackPanel a = s.Child as StackPanel;
+            Button b = a.Children[0] as Button;
+            b.Background = new SolidColorBrush(Colors.Red);
+
+            if ((Keyboard.GetKeyStates(Key.Down) & KeyStates.Down) > 0)
+            {
+                Move_Down();
+            }
+            
+
+        }
+        private static void Move_Up() { }
+        private static void Move_Down() 
+        {
+            MessageBox.Show("Pidoras");
+        }
+        private static void Move_Left() { }
+        private static void Move_Right() { }
+
+    }
+}
--- TaskManager/Test.cs
+++ TaskManager/Test.cs
@@ -13,31 +13,49 @@ namespace TaskManager
 {
     internal class Test
     {
+        public StackPanel myStackPanel;
         public static ScrollViewer Draw_Stack()
         {
+            var a = new Test();
             var myScrollViewer = new ScrollViewer
             {
                 HorizontalScrollBarVisibility = ScrollBarVisibility.Auto
             };
 
-            myScrollViewer.Content = MyBorder();
+            myScrollViewer.Content = a.MyBorder();
 
             return myScrollViewer;
         }
 
-        private static StackPanel MyBorder()
+        private StackPanel MyBorder()
         {
-            StackPanel myStackPanel = new StackPanel();
+            myStackPanel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
 
-            myStackPanel.Orientation = Orientation.Horizontal;
+            myStackPanel = Stack(myStackPanel);
+            var move_button = new Button
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                Content = "Move Card"
+            };
+            move_button.Click += Move_button_Click;
+            myStackPanel.Children.Add(move_button);
 
-            return Stack(myStackPanel);
+            return myStackPanel;
+        }
+
+        private void Move_button_Click(object sender, RoutedEventArgs e)
+        {
+            Moving_cards.Move(myStackPanel);
         }
 
         private static StackPanel Stack( StackPanel stack)
         { 
             stack.Children.Add(Column());
-            stack.Children.Add(Column());
+            stack.Children.Add(Jopumn());
             return stack;
         }
         private static Border Column()
@@ -55,27 +73,61 @@ namespace TaskManager
 
             StackPanel myStack = new StackPanel();
 
-            var myCart = new Button
+            var WhatToDo = new Button
             {
                 Background = new SolidColorBrush(Colors.Blue),
                 Margin = new Thickness(10),
-                Width = 200,
+                Width = 225,
                 Height = 40,
                 Content = "Сделать покушать!"
             };
 
-            var button_Add = new Button
+            var Plus = new Button
             {
                 Margin = new Thickness(20),
-                Width = 200,
+                Width = 225,
                 Height = 40,
                 Content = "+ Карточка"
             };
 
-            myStack.Children.Add(myCart);
-            myStack.Children.Add(button_Add);
+            myStack.Children.Add(WhatToDo);
+            myStack.Children.Add(Plus);
             bord.Child = myStack;
+
             return bord;
+        }
+        private static Border Jopumn()
+        {
+            Border Man = new Border
+            {
+                Margin = new Thickness(10),
+                Background = new SolidColorBrush(Colors.White),
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                Padding = new Thickness(3),
+                CornerRadius = new CornerRadius(5),
+                Effect = new DropShadowEffect { BlurRadius = 30, Color = Colors.Black, ShadowDepth = 0 }
+            };
+            StackPanel Bruh = new StackPanel();
+
+            Button AddJopumn = new Button
+            {
+                Background = new SolidColorBrush(Colors.Blue),
+                Margin = new Thickness(10),
+                Width = 50,
+                Height = 40,
+                Content = "+"
+            };
+            AddJopumn.Click += new RoutedEventHandler(AddJopumn_Click);
+
+            Bruh.Children.Add(AddJopumn);
+            Man.Child = Bruh;
+            return Man;
+
+        }
+        private static void AddJopumn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi!");
         }
     }
 }
