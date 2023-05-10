ac205411d4c6b3d3aababef12da3245828b72d61
Ярослав Юманов <yumanov.yi@students.dvfu.ru>
2023-05-10 21:51:18 +1000

    Многа сделал

---
+++ TaskManager/TaskWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskWindow.xaml
+    /// </summary>
+    public partial class TaskWindow : Window
+    {
+        public TaskWindow()
+        {
+            InitializeComponent();
+            var d = new Task();
+            Background = new SolidColorBrush(Colors.LightGray);
+            Content = d.Draw_Stack(this);
+            Show();
+        }
+    }
+}
